db699c28230c25c74efc2194201c1ffba69f7900
masa <nishikawa.masa104@gmail.com>
2024-03-30 21:50:40 +0900

    how many minutes left

--- now.py
+++ now.py
@@ -1,8 +1,14 @@
 from datetime import datetime
+import math
 
-conduct_date = datetime(2025, 4, 13)
+sleep_t = datetime(2023, 1, 1, 22, 0, 0)
+wakeup_t = datetime(2023, 1, 2, 8, 10, 0)
 
-now = datetime.now()
+delta = wakeup_t - sleep_t
+print(type(delta))
+sec = delta.seconds
+hours = sec / (60*60)
+hours_integer = math.floor(hours)
+minutes = round((hours - hours_integer)*60)
 
-delta = conduct_date - now
-print('あと'+str(delta.days+1)+'日')
+print('睡眠時間は'+str(hours_integer)+'時間'+str(minutes)+'分')
