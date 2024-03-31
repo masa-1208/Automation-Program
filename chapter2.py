8e688073aa5cdd21d2bbfb3b9cddc45d1ce58451
masa <nishikawa.masa104@gmail.com>
2024-03-31 15:34:24 +0900

    age_list ver1

--- chapter2.py
+++ chapter2.py
@@ -1,11 +1,17 @@
 import openpyxl as excel
+import datetime
 
 book = excel.Workbook()
 sheet = book.active
 
-for i in range(1,10):
-    for j in range(1,10):
-        cell = sheet.cell(row=i,column=j)
-        cell.value = cell.coordinate
+this_year = datetime.date.today().year
 
-book.save("test_coordinate.xlsx")
+for i in range(81):
+    age = i
+    birth_year = this_year - age
+    age_cell = sheet.cell(i+1, 1)
+    age_cell.value = 'age:' + str(age)
+    year_cell = sheet.cell(i+1, 2)
+    year_cell.value = 'birth year:' + str(birth_year)
+
+book.save("agelist.xlsx")
